73cdc2dd981a1f2f6cc9635b9484904229086911
yujin-zero <poly30@naver.com>
2024-09-29 21:37:18 +0900

    패키지

---
+++ src/OOP/com/info/dept/TestB.java
@@ -0,0 +1,8 @@
+package OOP.com.info.dept;
+
+public class TestB {
+    public void dispMessage() {
+        System.out.println("패키지 : "+"com.info.dept");
+        System.out.println("클래스 : "+"TestB");
+    }
+}
---
+++ src/OOP/com/info/dept/TestMain.java
@@ -0,0 +1,13 @@
+package OOP.com.info.dept;
+
+public class TestMain {
+    public static void main(String[] args) {
+        TestA ta = new TestA();
+        ta.dispMessage();
+
+        System.out.println();
+
+        TestB tb = new TestB();
+        tb.dispMessage();
+    }
+}
